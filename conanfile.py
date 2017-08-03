e4eb9feca5b3200193afb876cbf6d42007428253
Jerry W <jerrywiltse@gmail.com>
2017-08-03 11:50:26 -0400

    switched to level5group requirement

--- conanfile.py
+++ conanfile.py
@@ -7,28 +7,15 @@ class BoostFusionConan(ConanFile):
     url = "https://github.com/boostorg/fusion"
     description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
-    lib_short_name = "fusion"
-    requires =  "Boost.Config/1.64.0@bincrafters/testing", \
-                      "Boost.Core/1.64.0@bincrafters/testing", \
-                      "Boost.Function_Types/1.64.0@bincrafters/testing", \
-                      "Boost.Functional/1.64.0@bincrafters/testing", \
-                      "Boost.Mpl/1.64.0@bincrafters/testing", \
-                      "Boost.Preprocessor/1.64.0@bincrafters/testing", \
-                      "Boost.Static_Assert/1.64.0@bincrafters/testing", \
-                      "Boost.Tuple/1.64.0@bincrafters/testing", \
-                      "Boost.Type_Traits/1.64.0@bincrafters/testing",\
-                      "Boost.Typeof/1.64.0@bincrafters/testing",\
-                      "Boost.Utility/1.64.0@bincrafters/testing"
-
-                      #config0 core2 function_types5 functional5 mpl5 preprocessor0 static_assert1 tuple4 type_traits3 typeof5 utility5
-
-    def source(self):
-        self.run("git clone --depth=50 --branch=boost-{0} {1}.git"
-                 .format(self.version, self.url))
-
-    def package(self):
-        include_dir = os.path.join(self.build_folder, self.lib_short_name, "include")
-        self.copy(pattern="*", dst="", src=include_dir)
-
+    requires =  "Boost.Level5Group/1.64.0@bincrafters/testing"
+   
     def package_id(self):
         self.info.header_only()
+        
+    #This library is part of one or more cyclic dependency groups within Boost.
+    
+    #All members of cyclic dependency groups must be built under single package per group for Conan.
+    
+    #The combination is performed in the package(s) listed in the requires field.
+    
+    #This package enables simple consumption of this library while abstracting away the cyclic dependency issues. 
